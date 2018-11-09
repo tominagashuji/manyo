7cdf5160d7cbb0e9e9cf591124a5e95654d5c261
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2018-11-10 07:48:05 +0900

    step7タスクのcrud実装

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  before_action :set_task, only:[:show,:edit,:update,:destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to new_task_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path, notice: "タスク編集済み"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"タスク削除済み"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name,:content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
