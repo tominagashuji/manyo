9dccf23d5161e58a46a33a151fc9579ae4ee6df6
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2018-11-28 16:53:41 +0900

    微調整

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2018_11_28_074314) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.string "content"
+    t.string "name", null: false
+    t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
