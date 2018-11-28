842d9d903b78cc854802d78ba666be42a6bc81d6
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2018-11-28 17:24:25 +0900

    動作確認

--- db/migrate/20181128074314_cange_colmn_null_tasks.rb
+++
@@ -1,4 +0,0 @@
-class CangeColmnNullTasks < ActiveRecord::Migration[5.2]
-  change_column(:tasks, :name, :string, :null => false)
-  change_column(:tasks, :content, :string, :null => false)
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_074314) do
+ActiveRecord::Schema.define(version: 2018_11_09_121443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "content", null: false
+    t.string "name"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
