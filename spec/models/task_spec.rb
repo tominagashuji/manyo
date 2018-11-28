530fc7a87dddc6add7d154f221a13747c0a4b7f3
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2018-11-28 14:01:06 +0900

    step12_モデルのバリデーション

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+
+  it "nameが空ならバリデーションが通らない" do
+    task = Task.new(name: '', content: '失敗テスト')
+    expect(task).not_to be_valid
+  end
+
+  it "contentが空ならバリデーションが通らない" do
+    task = Task.new(name: '失敗テスト', content: '')
+    expect(task).not_to be_valid
+  end
+
+  it "nameとcontentに内容が記載されていればバリデーションが通る" do
+    task = Task.new(name: '成功テスト', content: '成功テスト')
+    expect(task).to be_valid
+  end
+end
