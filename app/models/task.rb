fa698c22f27103100090c9ffdbb31f3a58942bfe
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2019-01-06 07:04:26 +0900

    statusカラムと検索用indexの追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,10 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :content, presence: true
 
-  enum status: [:xxx, :bbb, :ccc]
+  # ステップ15対応 enum status: [:xxx, :bbb, :ccc]
+  enum status: {
+    nowork: 0, # 未着手
+    work: 1,   # 着手
+    comp: 2,  # 完了
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_095037) do
+ActiveRecord::Schema.define(version: 2019_01_05_214653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 2018_12_18_095037) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "limit_on", default: "1900-01-01", null: false
-    t.string "state", default: "未着手", null: false
     t.integer "status", default: 0, null: false
+    t.index ["name"], name: "index_tasks_on_name"
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
