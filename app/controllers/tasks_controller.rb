448a64a21a8655031fa818e7e8a19765fcf1ae37
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2019-01-06 10:12:18 +0900

    view/task/newのstatus修正、ストロングパラメータ修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -55,7 +55,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name,:content,:limit_on)
+    params.require(:task).permit(:name,:content,:limit_on,:status)
   end
 
   def set_task
