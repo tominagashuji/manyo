82aec62edfd027197cdc22c56b4f265bf13d4a63
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2019-01-26 09:14:01 +0900

    modelテストの実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  #事前にデータを用意しようとしたが上手く行かないので一旦保留
+  # before do
+  #   @task = FactoryBot.create(:task01)
+  # end
 
   it "nameが空ならバリデーションが通らない" do
     task = Task.new(name: '', content: '失敗テスト')
@@ -16,4 +20,30 @@ RSpec.describe Task, type: :model do
     task = Task.new(name: '成功テスト', content: '成功テスト')
     expect(task).to be_valid
   end
+
+  it "name検索テスト" do
+    # task01 = @task
+    task01 = Task.create(name: 'name01', content: 'content01', )
+    task02 = Task.create(name: 'name02', content: 'content02', )
+    task03 = Task.create(name: 'name33', content: 'content33', )
+    tasks = Task.name_search("name0")
+    expect(tasks).to include task01, task02
+  end
+
+  it "status検索テスト" do
+    task01 = Task.create(name: 'name01', content: 'content01', status: 'nowork')
+    task02 = Task.create(name: 'name02', content: 'content02', status: 'work')
+    task03 = Task.create(name: 'name33', content: 'content33', status: 'comp')
+    tasks = Task.status_search("nowork")
+    expect(tasks).to include task01
+  end
+
+  it "name,status検索テスト" do
+    task01 = Task.create(name: 'name01', content: 'content01', status: 'nowork')
+    task02 = Task.create(name: 'name02', content: 'content02', status: 'work')
+    task03 = Task.create(name: 'name03', content: 'content33', status: 'comp')
+    tasks = Task.name_status_search("name0","work")
+    expect(tasks).to include task02
+  end
+
 end
