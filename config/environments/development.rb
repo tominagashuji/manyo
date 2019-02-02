a9929400149337fafb9afbf28bf27eace6a27ad3
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2019-02-03 07:22:34 +0900

    管理ユーザーのみ管理画面アクセス可能対応

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  # step22 にてadmin user以外はエラーにする対応
+  config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
