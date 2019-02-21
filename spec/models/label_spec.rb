ff2c87567544a71aa15df9c39e5a75e33ed74bc5
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2019-02-21 11:19:04 +0900

    label_model テスト実装

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Label, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+  it "name が記載されればバリデーションが通る" do
+    label = Label.new(name: 'aaa')
+    expect(label).to be_valid
+  end
+
+  it "name が空はバリデーションが通らない" do
+    label = Label.new(name: '')
+    expect(label).not_to be_valid
+  end
+
+  it "name が４０文字以上はバリデーションが通らない" do
+    label =Label.new(name: 'a'*41)
+    expect(label).not_to be_valid
+  end
+
 end
